e1448a75a20397d36a7b01ef0569bba74d63f59e
Panagiotis Giagkoulas <pgiagkoulas@gmail.com>
2025-01-22 14:49:21 +0100

    Added and applied ruff to project

--- source/d1-LocationListsDistance.py
+++ source/d1-LocationListsDistance.py
@@ -1,6 +1,6 @@
 from utils import read_input_file
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Day 1:")
     input = read_input_file("d1-input.txt")
     list1 = []
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         nums = line.split("   ")
         list1.append(int(nums[0]))
         list2.append(int(nums[1]))
-    
+
     list1.sort()
     list2.sort()
     assert len(list1) == len(list2), "[!] Lists have uneven length!"
@@ -17,6 +17,5 @@ if __name__ == '__main__':
     distance = sum((abs(list1[i] - list2[i]) for i in range(len(list1))))
     print(f"[>] Total distance between the two lists: {distance}")
 
-    similarity = sum(num1 * sum((1 for num2 in list2 if num1==num2)) for num1 in list1)
+    similarity = sum(num1 * sum((1 for num2 in list2 if num1 == num2)) for num1 in list1)
     print(f"[>] Total similarity score between the two lists: {similarity}")
-
--- source/d3-MullItOver.py
+++ source/d3-MullItOver.py
@@ -4,10 +4,10 @@ import re
 
 def _get_operants(calculation: str) -> list[int]:
     calculation = calculation.replace("mul(", "").replace(")", "")
-    return [int(op) for op in calculation.split(',')]
+    return [int(op) for op in calculation.split(",")]
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("Day 3:")
     input = read_input_file("d3-input.txt")
     memory_contents = "".join(input)
@@ -15,12 +15,12 @@ if __name__=="__main__":
     regex_search_results = re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory_contents)
 
     mul_operants = [_get_operants(operation) for operation in regex_search_results]
-    mul_results = [op[0]*op[1] for op in mul_operants]
+    mul_results = [op[0] * op[1] for op in mul_operants]
     mul_sum = sum(mul_results)
     print(f"[>] Sum of multiplications: {mul_sum}")
 
     mul_cond_iterator = re.finditer(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))", memory_contents)
-    
+
     enabled = True
     enabled_mul_sum = 0
     for iter_result in mul_cond_iterator:
@@ -35,6 +35,5 @@ if __name__=="__main__":
                 enabled_mul_sum += operants[0] * operants[1]
         else:
             print("OPERATION PARSING PROBLEM - RAISING ERROR")
-            raise()
+            raise ()
     print(f"[>] Sum of multiplications with enables: {enabled_mul_sum}")
-
